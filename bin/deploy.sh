4694d884f85ee5a8768f74de6bd57ca7d814adbf
Joey Zhao <zhaoyi0113@gmail.com>
2019-07-19 14:41:07 +1000

    change to docker on building lambda

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,10 @@
 cd crawler
 rm -fr dist
-pip install -r requirements.txt -t dist
+mkdir dist
+# pip install -r requirements.txt -t dist
 cp src/*.py dist
 cd dist
+docker run -v $(pwd):/outputs -it --rm lambci/lambda:build-python3.6 pip install scrapy -t /outputs/
 # zip -r deploy.zip *
 cd ../..
 terraform apply
