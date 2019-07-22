8b53a0aae588cf18d928662dbc255d8a18cc2af7
Joey Zhao <zhaoyi0113@gmail.com>
2019-07-22 16:56:41 +1000

    update

--- crawler/src/praw_crawler.py
+++ crawler/src/praw_crawler.py
@@ -5,9 +5,12 @@ import boto3
 from datetime import datetime
 from io import StringIO
 
+LIMIT = 1000
+
 reddit = praw.Reddit(client_id='W52YZGBme79BpA',
                      client_secret='LBrMnomaVlNUI_PXcvMqPOSQtt4',
                      user_agent='my user agent')
+print('token:', reddit.auth.scopes())
 
 def upload_to_s3(df):
     csv_buffer = StringIO()
@@ -19,18 +22,15 @@ def upload_to_s3(df):
 def handler(event, context):
     print(reddit.read_only)
     info = []
-    topics = reddit.subreddit('NetflixBestOf').new(limit=1000)
+    topics = reddit.subreddit('NetflixBestOf').new(limit=LIMIT)
     for submission in topics:
         print(submission.title)
-        # info=np.append(info, {'id': submission.id, 'name': submission.name,
-        #              'num_comments': submission.num_comments, 'vote': submission.score, 'title': submission.title, 'url': submission.url})
         row = np.array([submission.id, submission.name, submission.num_comments,
                         submission.score, submission.title, submission.url])
         info.append(row)
     np_info = np.array(info)
     columns = ['id', 'name', 'num_comments', 'vote', 'title', 'url']
     df = pd.DataFrame(np_info, columns=columns)
-    # print('return:', df)
     print(df.head())
     upload_to_s3(df)
     return info
