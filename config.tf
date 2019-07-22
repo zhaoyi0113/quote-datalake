a1ee88cd8673c77e97e3e98842dd1c7e7e6cb46b
Joey Zhao <zhaoyi0113@gmail.com>
2019-07-22 21:45:08 +1000

    make terraform zip and upload lambda to s3 bucket

--- config.tf
+++ config.tf
@@ -15,13 +15,41 @@ variable "runtime" {
 variable "region" {
   default = "ap-southeast-2"
 }
+variable "s3-bucket" {
+  default = "jzhao-datalake-test"
+}
+
+variable "s3-crawler-deploy-key" {
+  default = "deploy/crawler/deploy.zip"
+}
+
+variable "crawler_packaged_file" {
+  default = "crawler/dist/deploy.zip"
+}
+
+
 data "archive_file" "zipit" {
   type        = "zip"
   source_dir  = "crawler/dist"
-  output_path = "crawler/dist/deploy.zip"
+  output_path = "${var.crawler_packaged_file}"
 }
+
+resource "aws_s3_bucket_object" "file_upload" {
+  bucket = "${var.s3-bucket}"
+  key    = "${var.s3-crawler-deploy-key}"
+  source = "${data.archive_file.zipit.output_path}"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  # etag = "${filemd5("path/to/file")}"
+}
+
 resource "aws_lambda_function" "test_lambda" {
-  filename         = "crawler/dist/deploy.zip"
+  # filename         = "crawler/dist/deploy.zip"
+  s3_bucket = "${var.s3-bucket}"
+  s3_key    = "${aws_s3_bucket_object.file_upload.key}"
+  # source_code_hash = "${filebase64sha256("file.zip")}"
   function_name    = "quote-crawler"
   role             = "arn:aws:iam::773592622512:role/LambdaRole"
   handler          = "handler.handler"
@@ -37,7 +65,9 @@ resource "aws_lambda_function" "test_lambda" {
 }
 
 resource "aws_lambda_function" "praw_crawler" {
-  filename         = "crawler/dist/deploy.zip"
+  # filename         = "crawler/dist/deploy.zip"
+  s3_bucket        = "${var.s3-bucket}"
+  s3_key           = "${var.s3-crawler-deploy-key}"
   function_name    = "praw_crawler"
   role             = "arn:aws:iam::773592622512:role/LambdaRole"
   handler          = "praw_crawler.handler"
@@ -57,6 +87,7 @@ resource "aws_api_gateway_resource" "resource" {
   path_part   = "resource"
 }
 
+# API Gateway
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
   resource_id   = "${aws_api_gateway_resource.resource.id}"
@@ -87,5 +118,5 @@ resource "aws_lambda_permission" "apigw_lambda" {
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  #   source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+  # source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
 }
