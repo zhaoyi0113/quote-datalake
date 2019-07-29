5e18036fd43806cc6d74177133825358b473fa5d
Joey Zhao <zhaoyi0113@gmail.com>
2019-07-29 10:02:08 +1000

    add cloudwatch log group for crawler

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,29 +1,29 @@
 resource "aws_cloudwatch_event_rule" "every_one_minutes" {
-    name = "reddit_monitor_scheduler"
-    description = "Fires every one minutes"
-    schedule_expression = "rate(2 minutes)"
+  name                = "reddit_monitor_scheduler"
+  description         = "Fires every one minutes"
+  schedule_expression = "rate(2 minutes)"
 }
 
 resource "aws_cloudwatch_event_target" "search_movies_reddit_every_one_minutes" {
-    rule = "${aws_cloudwatch_event_rule.every_one_minutes.name}"
-    target_id = "search_movies_reddit"
-    arn = "${aws_lambda_function.reddit_montior.arn}"
-    input = "{\"topic\": \"movies\"}"
+  rule      = "${aws_cloudwatch_event_rule.every_one_minutes.name}"
+  target_id = "search_movies_reddit"
+  arn       = "${aws_lambda_function.reddit_montior.arn}"
+  input     = "{\"topic\": \"movies\"}"
 }
 
 resource "aws_cloudwatch_event_target" "search_netflixbestof_reddit_every_one_minutes" {
-    rule = "${aws_cloudwatch_event_rule.every_one_minutes.name}"
-    target_id = "search_netflixbestof_reddit"
-    arn = "${aws_lambda_function.reddit_montior.arn}"
-    input = "{\"topic\": \"NetflixBestOf\"}"
+  rule      = "${aws_cloudwatch_event_rule.every_one_minutes.name}"
+  target_id = "search_netflixbestof_reddit"
+  arn       = "${aws_lambda_function.reddit_montior.arn}"
+  input     = "{\"topic\": \"NetflixBestOf\"}"
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_reddit" {
-    statement_id = "AllowExecutionFromCloudWatch"
-    action = "lambda:InvokeFunction"
-    function_name = "${aws_lambda_function.reddit_montior.function_name}"
-    principal = "events.amazonaws.com"
-    source_arn = "${aws_cloudwatch_event_rule.every_one_minutes.arn}"
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.reddit_montior.function_name}"
+  principal     = "events.amazonaws.com"
+  source_arn    = "${aws_cloudwatch_event_rule.every_one_minutes.arn}"
 }
 
 ## add event to listen on glue crawler event
@@ -55,4 +55,17 @@ resource "aws_cloudwatch_event_target" "trigger_glue_job_lambda" {
   rule = "${aws_cloudwatch_event_rule.crawler.name}"
   target_id = "SendToLambda"
   arn = "${aws_lambda_function.trigger_glue_job.arn}"
+}
+
+resource "aws_cloudwatch_event_target" "log_glue_crawler_result" {
+  rule = "${aws_cloudwatch_event_rule.crawler.name}"
+  target_id = "LogCrawler"
+  arn = "${aws_cloudwatch_log_group.grue_crawler_log_group.arn}"
+}
+
+resource "aws_cloudwatch_log_group" "grue_crawler_log_group" {
+  name = "crawler_log"
+  tags = {
+    Application = "reddit_movies"
+  }
 }
